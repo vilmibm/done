70df7148a9378228098b604a9001410b0ba70f3c
Nathaniel Smith <nathanielksmith@gmail.com>
2010-04-05 19:56:54 -0400

    update version number

--- done/__init__.py
+++ done/__init__.py
@@ -1,5 +1,5 @@
 __author__ = 'Nathaniel K Smith <nathanielksmith@gmail.com>'
-__version__ = (1, 0, 2)
+__version__ = (1, 0, 3)
 
 def get_version():
     return '.'.join(str(bit) for bit in __version__)
