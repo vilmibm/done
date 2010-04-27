f0ec4da428e7254c47d20414adea7cbfa5006929
aezell <alex@myemma.com>
2010-04-27 13:28:41 -0500

    Fix passing the unparsed due date.

--- done/Commands.py
+++ done/Commands.py
@@ -32,7 +32,7 @@ def run(command, options, args):
         dp = pdt.Calendar()
         due = mktime(dp.parse(options.due)[0]) if options.due else None
         print "added tasks..."
-        [Task(desc, options.due).add() for desc in args]
+        [Task(desc, due).add() for desc in args]
         return
 
     filters = args if len(args) else None
